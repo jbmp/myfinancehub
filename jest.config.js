204297ab12f6a8bba86c3149530bc841abd1725f
Jorge Brás <jorzx1@hotmail.com>
2023-09-10 23:23:18 +0100

    configura eslint, tsconfig and jest

---
+++ jest.config.js
@@ -0,0 +1,32 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  clearMocks: true,
+
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/src/db/',
+  ],
+
+  coverageProvider: 'v8',
+
+  coverageReporters: [
+    'json',
+    'text',
+    'lcov',
+    'clover',
+  ],
+  moduleFileExtensions: [
+    'js',
+    'ts',
+    'tsx',
+  ],
+  roots: [
+    '<rootDir>/src',
+  ],
+  preset: 'ts-jest',
+  testMatch: ["**/src/**/*.test.ts"],
+  testEnvironment: 'node'
+};
